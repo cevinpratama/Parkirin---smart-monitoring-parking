c93f83fcdf35accc28bb64d957f4ced2ba780937
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-27 21:40:05 +0700

    membuat route

--- app/src/main/java/com/d3ifcool/parkirin/MainActivity.kt
+++ app/src/main/java/com/d3ifcool/parkirin/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.d3ifcool.parkirin.ui.theme.ParkirINTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ParkirINTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ParkirINApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ParkirINTheme {
-        Greeting("Android")
+fun ParkirINApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination =" "
+    ){
+        composable("splashPage") {
+            splashPage(navController = navController)
+        }
     }
 }
