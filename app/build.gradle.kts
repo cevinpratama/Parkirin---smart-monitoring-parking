2a5a58309ca96e4ad29a83939a8b3c0bced32989
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-27 21:39:33 +0700

    menambahkan gradle awal yang dibutuhkan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.navigation.compose)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
