07a44cbd5322f9a93bab90a9af77e8ec81a05ef4
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-28 20:44:39 +0700

    membuat tampilan splashPage.kt

--- app/src/main/java/com/d3ifcool/parkirin/splashPage.kt
+++ app/src/main/java/com/d3ifcool/parkirin/splashPage.kt
@@ -1,9 +1,61 @@
 package com.d3ifcool.parkirin
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.d3ifcool.parkirin.ui.theme.BiruJ
+import com.d3ifcool.parkirin.ui.theme.Biruk
+import com.d3ifcool.parkirin.ui.theme.LucidaSansStyle
 
 @Composable
 fun splashPage(navController: NavController){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+                horizontalAlignment =Alignment.CenterHorizontally
+        ){
+
+            Spacer(modifier = Modifier.weight(2f))
+
+            Text(
+                text = "ParkirIn",
+                fontSize = 64.sp,
+                style = MaterialTheme.typography.bodyLarge,color = BiruJ
+            )
+
+            Spacer(modifier = Modifier.height(2.dp))
+
+            Text(
+                text = "temukan, parkir, lanjut",
+                fontSize = 19.sp,
+                style = LucidaSansStyle,color = Biruk
+            )
+
+            Spacer(modifier = Modifier.weight(2f))
+            Text(
+                text = "click anywhere",
+                fontSize = 12.sp,
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(100.dp))
+        }
+    }
 
 }
