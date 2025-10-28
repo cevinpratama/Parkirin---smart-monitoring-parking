f12112d5da0e21a73ade682102066a0fdee293fa
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-28 20:44:20 +0700

    configurasi font

--- app/src/main/java/com/d3ifcool/parkirin/ui/theme/Type.kt
+++ app/src/main/java/com/d3ifcool/parkirin/ui/theme/Type.kt
@@ -5,11 +5,30 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.d3ifcool.parkirin.R
 
-// Set of Material typography styles to start with
+
+val LilitaOneFamily = FontFamily(
+    Font(R.font.lilitaone_regular, FontWeight.Normal)
+
+)
+
+val LucidaSansFamily = FontFamily(
+    Font(R.font.l_10646)
+)
+
+val LucidaSansStyle = TextStyle(
+    fontFamily = LucidaSansFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = 0.5.sp
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = LilitaOneFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
