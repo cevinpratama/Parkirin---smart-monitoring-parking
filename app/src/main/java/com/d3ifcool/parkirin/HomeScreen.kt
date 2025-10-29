9a453f8bcd11784658b1bea38b384d9b28e334eb
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-29 18:34:34 +0700

    feat(HomeScreen): Menambahkan tampilan awal HomeScreen.kt

--- app/src/main/java/com/d3ifcool/parkirin/HomeScreen.kt
+++ app/src/main/java/com/d3ifcool/parkirin/HomeScreen.kt
@@ -1,9 +1,39 @@
 package com.d3ifcool.parkirin
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController){
 
+      val isDark = isSystemInDarkTheme()
+    val backgroundColor = if (isDark) Color(0xFF121212) else Color.White
+
+    Scaffold (
+        containerColor = backgroundColor,
+        topBar = {
+          TopBar(
+            title = "ParkirIn",
+          )
+        }
+    ){innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Konten Utama HomeScreen")
+
+    }
+}
 }
