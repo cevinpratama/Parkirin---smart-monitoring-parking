1898a159518d46e4d30dd00cee7c874cfb3b99a8
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-29 20:31:08 +0700

    refactor: Pindahkan string statis ke file resource

--- app/src/main/java/com/d3ifcool/parkirin/HomeScreen.kt
+++ app/src/main/java/com/d3ifcool/parkirin/HomeScreen.kt
@@ -19,13 +19,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.d3ifcool.parkirin.ui.theme.BiruJ
 
+
 @Composable
-fun HomeScreen(navController: NavController){
+fun HomeScreen(navController: NavController) {
 
     val isDark = isSystemInDarkTheme()
     val backgroundColor = if (isDark) Color(0xFF121212) else Color.White
@@ -33,14 +35,15 @@ fun HomeScreen(navController: NavController){
     val jumlah = 24
     val tersedia = 4
 
-    Scaffold (
+
+    Scaffold(
         containerColor = backgroundColor,
         topBar = {
-          TopBar(
-            title = "ParkirIn",
-          )
+            TopBar(
+                title = stringResource(R.string.app_name),
+            )
         }
-    ){innerPadding ->
+    ) { innerPadding ->
 
         Box(
             modifier = Modifier
@@ -49,52 +52,50 @@ fun HomeScreen(navController: NavController){
             contentAlignment = Alignment.TopCenter
         ) {
 
-            Column (
-
-            ){
+            Column {
 
                 Spacer(modifier = Modifier.height(24.dp))
 
-                Box (
+                Box(
                     modifier = Modifier
-                         .size(width = 320.dp, height = 287.dp)
-                         .clip(RoundedCornerShape(16.dp))
+                        .size(width = 320.dp, height = 287.dp)
+                        .clip(RoundedCornerShape(16.dp))
                         .background(color = BiruJ),
                     contentAlignment = Alignment.TopCenter
-                ){
+                ) {
 
                     Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                    ){
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
 
                         Spacer(modifier = Modifier.height(15.dp))
 
                         Text(
-                            text = "Slot Parkir",
+                            text = stringResource(R.string.home_parking_slot_title),
                             fontSize = 45.sp,
                             color = Color.White
                         )
 
                         Spacer(modifier = Modifier.height(50.dp))
 
-                        Row{
+                        Row {
                             Column(
                                 horizontalAlignment = Alignment.End
                             ) {
                                 Text(
-                                    text = "Jumlah",
+                                    text = stringResource(R.string.home_label_total),
                                     fontSize = 20.sp,
                                     color = Color.White
                                 )
 
                                 Text(
-                                    text = "Tersedia",
+                                    text = stringResource(R.string.home_label_available),
                                     fontSize = 20.sp,
                                     color = Color.White
                                 )
 
                                 Text(
-                                    text = "Status",
+                                    text = stringResource(R.string.home_label_status),
                                     fontSize = 20.sp,
                                     color = Color.White
                                 )
@@ -134,33 +135,30 @@ fun HomeScreen(navController: NavController){
                                     fontSize = 20.sp,
                                     color = Color.White
                                 )
-                              if(tersedia == 0)
-                                Text(
-                                    text = "Penuh",
-                                    fontSize = 20.sp,
-                                    color = Color.Red
-                                )
+                                if (tersedia == 0)
+                                    Text(
+                                        text = stringResource(R.string.home_status_full),
+                                        fontSize = 20.sp,
+                                        color = Color.Red
+                                    )
                                 else
-                                Text(
-                                    text = "Tersedia",
-                                    fontSize = 20.sp,
-                                    color = Color.Green
-                                )
+                                    Text(
+                                        text = stringResource(R.string.home_status_available),
+                                        fontSize = 20.sp,
+                                        color = Color.Green
+                                    )
                             }
                         }
-
-
-
                     }
-
                 }
 
+                Spacer(modifier = Modifier.height(16.dp))
 
-
-                Text(text = "Konten Utama HomeScreen")
+                Text(
+                    text = "Ini Adalah Main Screen"
+                )
 
             }
-
+        }
     }
-}
 }
